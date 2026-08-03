3c678df5411b1bc28544eabff6127934df5e7f14
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-03 07:39:10 +0000

    feat: openapi fetch用のclient定義

---
+++ frontend/src/shared/api/client.ts
@@ -0,0 +1,5 @@
+import createClient from "openapi-fetch";
+import type { paths } from "./schema";
+import { API_BASE_URL } from "../config/env";
+
+export const client = createClient<paths>({ baseUrl: API_BASE_URL });
