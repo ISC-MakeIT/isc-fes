c9f8267e45d99e986e5d112c3da4137ab439f570
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-04 03:49:51 +0000

    fix: 未使用でLint落ちるので削除

--- frontend/src/_pages/home/home-view.tsx
+++ frontend/src/_pages/home/home-view.tsx
@@ -1,12 +1,5 @@
 import { LoginButton } from "@/entities/account/ui/login-button";
-import { client } from "@/shared/api/client";
-import { cookies } from "next/headers";
 
 export async function HomeView() {
-  const cookieStore = await cookies();
-  const { data, error } = await client.GET("/me", {
-    headers: { Cookie: cookieStore.toString() },
-  });
-
   return <LoginButton />;
 }
