b608f1af673caaa12f33e7344a0eaa687d98fcb5
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-04 05:49:58 +0000

    style: format

--- frontend/src/entities/account/ui/login-button.tsx
+++ frontend/src/entities/account/ui/login-button.tsx
@@ -1,5 +1,5 @@
 import { API_BASE_URL } from "@/shared/config/env";
-import { Button, buttonVariants } from "@/shared/ui/button";
+import { buttonVariants } from "@/shared/ui/button";
 import Link from "next/link";
 
 const GOOGLE_LOGIN_ENDPOINT = API_BASE_URL + "/auth/google/login";
