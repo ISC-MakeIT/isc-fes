453b1d3864f87f1abf94e502320545beef859950
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-04 02:20:35 +0000

    fix: クッキー送れてなくて実際にログインできてなかった

--- frontend/src/_pages/home/home-view.tsx
+++ frontend/src/_pages/home/home-view.tsx
@@ -1,5 +1,12 @@
 import { LoginButton } from "@/entities/user/ui/login-button";
+import { client } from "@/shared/api/client";
+import { cookies } from "next/headers";
 
-export function HomeView() {
+export async function HomeView() {
+  const cookieStore = await cookies();
+  const { data, error } = await client.GET("/me", {
+    headers: { Cookie: cookieStore.toString() },
+  });
+
   return <LoginButton />;
 }
--- frontend/src/shared/api/client.ts
+++ frontend/src/shared/api/client.ts
@@ -2,4 +2,7 @@ import createClient from "openapi-fetch";
 import type { paths } from "./schema";
 import { API_BASE_URL } from "../config/env";
 
-export const client = createClient<paths>({ baseUrl: API_BASE_URL });
+export const client = createClient<paths>({
+  baseUrl: API_BASE_URL,
+  credentials: "include",
+});
