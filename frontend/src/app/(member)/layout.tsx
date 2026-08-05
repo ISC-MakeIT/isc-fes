6ac941d0fe57118aa672bb75fc3b67e5c137487f
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-05 08:25:39 +0000

    refacotr: loginページを(member)外に置いたのでなくていい

--- frontend/src/app/(member)/layout.tsx
+++ frontend/src/app/(member)/layout.tsx
@@ -2,16 +2,19 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import React from "react";
 
+const LOGIN_PAGE_URL = "/login";
+
+/**
+ * ログイン必須のページにアクセスしてきた人を弾く用のLayout
+ */
 export default async function MemberLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const baseUrl = location.origin;
-
   const sessionCookie = (await cookies()).get("isc_fes_account_session");
-  if (!sessionCookie && location.pathname !== "/login")
-    redirect("/login" + baseUrl);
+
+  if (!sessionCookie) redirect(LOGIN_PAGE_URL);
 
   return children;
 }
