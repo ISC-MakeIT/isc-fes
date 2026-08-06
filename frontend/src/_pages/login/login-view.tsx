21c6de3f236720d5d8bfa64581743a5652ff3214
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-06 08:28:11 +0000

    style: レスポンシブ

--- frontend/src/_pages/login/login-view.tsx
+++ frontend/src/_pages/login/login-view.tsx
@@ -4,7 +4,7 @@ export async function LoginView() {
   return (
     <div className="m-auto space-y-9 text-center font-bold">
       {/* TODO: アプリ名が決まったら書き換える */}
-      <h1 className="text-6xl">アプリ名があれば</h1>
+      <h1 className="text-4xl sm:text-6xl">アプリ名があれば</h1>
       <LoginButton />
     </div>
   );
