3e7c6be962d2ed0cd7417d1acfebeded3b7f54df
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-06 05:48:31 +0000

    feat: コメント追加

--- frontend/src/app/(member)/layout.tsx
+++ frontend/src/app/(member)/layout.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 
 /**
  * ログイン必須のページにアクセスしてきた人を弾く用のLayout
+ * TTFBが伸びる関係でここではsessionだけを見た楽観的なアクセス制限を行っている
  */
 export default async function MemberLayout(props: LayoutProps<"/">) {
   const sessionCookie = (await cookies()).get(SESSION_COOKIE_NAME);
