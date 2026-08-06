7186da21658b53a326f45347ba62074bc1036666
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-06 12:35:46 +0000

    wip: ページとルート用意

---
+++ frontend/src/app/(member)/store-list/page.tsx
@@ -0,0 +1,5 @@
+import { StoreListView } from "@/_pages/store-list/store-list-view";
+
+export default function StoreListPage() {
+  return <StoreListView />;
+}
--- frontend/src/shared/config/constants/urls.ts
+++ frontend/src/shared/config/constants/urls.ts
@@ -1,1 +1,3 @@
 export const LOGIN_URL = "/login";
+export const STORE_LIST_URL = "/store-list";
+export const REGISTER_STORE_URL = "/register-store";
