54274fbd1697737e2089ef810bba2da7cd23a0a4
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-06 05:47:18 +0000

    fix: layout ぷrパティヘルパー使用

--- frontend/src/app/(member)/layout.tsx
+++ frontend/src/app/(member)/layout.tsx
@@ -2,19 +2,14 @@ import { SESSION_COOKIE_NAME } from "@/shared/config/cookies";
 import { LOGIN_URL } from "@/shared/config/urls";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import React from "react";
 
 /**
  * ログイン必須のページにアクセスしてきた人を弾く用のLayout
  */
-export default async function MemberLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function MemberLayout(props: LayoutProps<"/">) {
   const sessionCookie = (await cookies()).get(SESSION_COOKIE_NAME);
 
   if (!sessionCookie) redirect(LOGIN_URL);
 
-  return children;
+  return props.children;
 }
