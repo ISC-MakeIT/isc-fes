7b735846171f318e1bcb1bb5b0e856332074935f
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-12 16:01:16 +0900

    fix: DotTextの中はspanなのでh1の中に配備

--- frontend/src/shared/ui/heading-card.tsx
+++ frontend/src/shared/ui/heading-card.tsx
@@ -8,9 +8,9 @@ type HeadingCardProps = {
 export function HeadingCard({ children }: HeadingCardProps) {
   return (
     <Card className="bg-primary mx-auto rounded-sm px-6 py-2 text-center text-2xl text-white shadow-[7px_7px_0_rgb(254,218,62)]">
-      <DotText>
-        <h1>{children}</h1>
-      </DotText>
+      <h1>
+        <DotText>{children}</DotText>
+      </h1>
     </Card>
   );
 }
