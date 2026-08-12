c6d2c016b45d23cffb9e8d7bab9be0c43b15d6eb
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-12 14:25:54 +0900

    refactor: 他のコンポーネントと統一

--- frontend/src/shared/ui/heading-card.tsx
+++ frontend/src/shared/ui/heading-card.tsx
@@ -5,7 +5,7 @@ type HeadingCardProps = {
   children: React.ReactNode;
 };
 
-export default function HeadingCard({ children }: HeadingCardProps) {
+export function HeadingCard({ children }: HeadingCardProps) {
   return (
     <Card className="bg-primary mx-auto rounded-sm px-6 py-2 text-center text-2xl text-white shadow-[7px_7px_0_rgb(254,218,62)]">
       <DotText>
