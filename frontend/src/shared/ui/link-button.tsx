6b7796bb1aeac378324e8b79e08e3a860a1e924e
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-13 17:45:42 +0900

    feat: リンクボタンの見た目を汎用的に

--- frontend/src/shared/ui/link-button.tsx
+++ frontend/src/shared/ui/link-button.tsx
@@ -4,13 +4,17 @@ import { cn } from "../lib/utils";
 import { buttonVariants } from "./button";
 
 type LinkButtonProps = ComponentProps<typeof Link> & {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export function LinkButton({ className, children, ...props }: LinkButtonProps) {
   return (
     <Link
-      className={cn(buttonVariants(), className, "hover:cursor-pointer")}
+      className={cn(
+        buttonVariants(),
+        "px-8 py-7 text-2xl shadow-[3px_3px_0_#9683DC]",
+        className,
+      )}
       {...props}
     >
       {children}
