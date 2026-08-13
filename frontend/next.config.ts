064076e42697f0af25197b012f90e65d3ffc5580
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-13 19:51:28 +0900

    feat: 本番の画像ストレージを取得できるように

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         port: "4566",
         pathname: "/isc-fes-local/stores/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.fes.iwasaki.ac.jp",
+      },
     ],
     // 開発環境ではLocalの画像ストレージを使うため内部IPへの接続を許可してる
     dangerouslyAllowLocalIP: isDev,
