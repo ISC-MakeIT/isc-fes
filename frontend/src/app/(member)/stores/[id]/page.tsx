2aed00c89ec4e6e51950921232b982488208889f
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-18 11:08:12 +0900

    refactor: ヘルパーを使用

--- frontend/src/app/(member)/stores/[id]/page.tsx
+++ frontend/src/app/(member)/stores/[id]/page.tsx
@@ -1,10 +1,8 @@
 import { StoreHomeView } from "@/_pages/store-home";
 
-type StoreHomePageProps = {
-  params: Promise<{ id: string }>;
-};
-
-export default async function StoreHomePage({ params }: StoreHomePageProps) {
+export default async function StoreHomePage({
+  params,
+}: PageProps<"/stores/[id]">) {
   const { id } = await params;
   return <StoreHomeView storeId={id} />;
 }
