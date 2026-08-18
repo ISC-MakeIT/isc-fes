ae28258f1d7881d1e09599d618318106cd798eae
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-18 15:36:37 +0900

    feat: URL定義

--- frontend/src/shared/config/constants/urls.ts
+++ frontend/src/shared/config/constants/urls.ts
@@ -3,3 +3,7 @@ export const storeListUrl = () => "/stores";
 export const registerStoreUrl = () => "/stores/register";
 
 export const storeHomeUrl = (id: string) => `/stores/${id}`;
+export const storeKitchenUrl = (id: string) => `/stores/${id}/kitchen`;
+export const storePickupUrl = (id: string) => `/stores/${id}/pickup`;
+export const storeCallUrl = (id: string) => `/stores/${id}/call`;
+export const storeMenusUrl = (id: string) => `/stores/${id}/menus`;
