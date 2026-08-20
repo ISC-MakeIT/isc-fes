5d86add6e7ee2f16ea78e17a5d98b25a76c3be60
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-20 17:51:47 +0900

    fix: 自分自身もメンバーとして表示

--- frontend/src/_pages/store-home/ui/store-member-list.tsx
+++ frontend/src/_pages/store-home/ui/store-member-list.tsx
@@ -8,7 +8,6 @@ import { Card } from "@/shared/ui/card";
 import { Button } from "@/shared/ui/button";
 import { SubmitButton } from "@/shared/ui/submit-button";
 import { PlusIcon } from "lucide-react";
-import { currentAccountQueryOptions } from "@/entities/account";
 import { AspectRatioImage } from "@/shared/ui/aspect-ratio-image";
 import { ICON_IMAGE_ASPECT } from "@/shared/config";
 
@@ -20,28 +19,17 @@ type StoreMemberListProps = {
 
 export function StoreMemberList({ storeId }: StoreMemberListProps) {
   const { data: members } = useSuspenseQuery(storeMemberQueryOptions(storeId));
-  const { data: currentAccount } = useSuspenseQuery(
-    currentAccountQueryOptions(),
-  );
-
-  const otherMembers = members.filter((m) => m.accountId !== currentAccount.id);
 
   return (
     <div className="border-primary flex flex-col gap-8 px-13 py-8 md:border-l md:px-6">
       <HeadingCard className="self-center">メンバー</HeadingCard>
-      {otherMembers.length === 0 ? (
-        <p>
-          メンバーがいません。
-          <br />
-          招待リンクをコピーして、メンバーを招待しましょう！
-        </p>
-      ) : (
-        <div>
-          {otherMembers.map((m) => (
-            <MemberCard key={m.accountId} member={m} />
-          ))}
-        </div>
-      )}
+
+      <div>
+        {members.map((m) => (
+          <MemberCard key={m.accountId} member={m} />
+        ))}
+      </div>
+
       <SubmitButton className="py-2 text-lg font-bold" isDot={false}>
         <PlusIcon className="size-6" />
         メンバーを招待
