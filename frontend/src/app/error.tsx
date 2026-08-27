6c0abefc84fd902d86eb98be588706e5296a4784
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-27 22:15:46 +0900

    refactor: まだ分けるメリットなさそうなのでappに移す

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ErrorView } from "@/shared/ui/error-view";
+import { SubmitButton } from "@/shared/ui/submit-button";
 import { useRouter } from "next/navigation";
 import { startTransition } from "react";
 
@@ -20,5 +20,27 @@ export default function Error({ reset }: ErrorProps) {
     });
   }
 
-  return <ErrorView onRetry={handleRetry} />;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10">
+      <div className="flex flex-col gap-4 text-center">
+        <h1 className="text-primary text-shadow-secondary text-9xl font-bold text-shadow-[4px_4px_0]">
+          Error
+        </h1>
+
+        <p className="text-xl">
+          予期せぬエラーが発生しました。
+          <br />
+          時間をおいて再度お試しください。
+        </p>
+      </div>
+
+      <SubmitButton
+        isDot={false}
+        className="px-10 py-4 text-2xl font-bold"
+        onClick={handleRetry}
+      >
+        リトライ
+      </SubmitButton>
+    </div>
+  );
 }
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,5 +1,27 @@
-import { NotFoundView } from "@/shared/ui/not-found-view";
+import { homeUrl } from "@/shared/config";
+import { LinkButton } from "@/shared/ui/link-button";
+import { ChevronLeft } from "lucide-react";
 
 export default function NotFound() {
-  return <NotFoundView />;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 text-center">
+      <h1 className="text-primary text-shadow-secondary text-9xl font-bold text-shadow-[4px_4px_0]">
+        404
+      </h1>
+      <h2 className="text-4xl font-bold">
+        お探しのページは見つかりませんでした。
+      </h2>
+      <p className="text-xl font-medium">
+        アクセスされたページは削除されたか
+        <br />
+        公開期限が過ぎた可能性があります。
+        <br />
+        お手数ですが、トップページより再度お探しください。
+      </p>
+      <LinkButton className="text-2xl font-bold" href={homeUrl()}>
+        <ChevronLeft className="size-7" />
+        トップに戻る
+      </LinkButton>
+    </div>
+  );
 }
