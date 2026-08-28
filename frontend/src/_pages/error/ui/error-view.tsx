f31382dea6322d9ae7f6cb4dd0fe591d53229199
ryotaKFC <ryotamokatoka@gmail.com>
2026-08-28 12:37:03 +0900

    feat: モバイル対応&左右の余白追加

--- frontend/src/_pages/error/ui/error-view.tsx
+++ frontend/src/_pages/error/ui/error-view.tsx
@@ -6,13 +6,13 @@ type ErrorViewProps = {
 
 export function ErrorView({ retryFunction }: ErrorViewProps) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-10">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-10 px-3">
       <div className="flex flex-col gap-4 text-center">
-        <h1 className="text-primary text-shadow-secondary text-9xl font-bold text-shadow-[4px_4px_0]">
+        <h1 className="text-primary text-shadow-secondary text-8xl font-bold text-shadow-[4px_4px_0] md:text-9xl">
           Error
         </h1>
 
-        <p className="text-xl">
+        <p className="text-base md:text-xl">
           予期せぬエラーが発生しました。
           <br />
           時間をおいて再度お試しください。
@@ -21,7 +21,7 @@ export function ErrorView({ retryFunction }: ErrorViewProps) {
 
       <SubmitButton
         isDot={false}
-        className="px-10 py-4 text-2xl font-bold"
+        className="px-10 py-4 text-base font-bold md:text-2xl"
         onClick={retryFunction}
       >
         リトライ
