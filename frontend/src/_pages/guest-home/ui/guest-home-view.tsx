59152982eb9786025f1c9611fb99b7d5a543a0bb
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-04 21:09:48 +0900

    style: スマホ対応

--- frontend/src/_pages/guest-home/ui/guest-home-view.tsx
+++ frontend/src/_pages/guest-home/ui/guest-home-view.tsx
@@ -4,13 +4,12 @@ import { FloorGuide } from "./floor-guide";
 
 export function GuestHomeView() {
   return (
-    <div className="space-y-8">
-      <section>
-        {/* TODO: キービジュアルができしだい配備 */}
-        <AspectRatio ratio={HERO_IMAGE_ASPECT} className="bg-gray-300" />
-
+    <div className="space-y-8 md:flex md:flex-row">
+      {/* TODO: キービジュアルができしだい配備 */}
+      <AspectRatio ratio={HERO_IMAGE_ASPECT} className="bg-gray-300 md:w-140" />
+      <div className="mx-auto">
         <FloorGuide />
-      </section>
+      </div>
     </div>
   );
 }
