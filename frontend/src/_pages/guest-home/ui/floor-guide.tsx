7262217669b24bd6cb7a2ca9c5bf8f62787f6e55
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-05 19:18:29 +0900

    fix: clickableな画像は動かないように & 名前が広すぎた

--- frontend/src/_pages/guest-home/ui/floor-guide.tsx
+++ frontend/src/_pages/guest-home/ui/floor-guide.tsx
@@ -13,22 +13,22 @@ import floor8Image from "./assets/floor-8f.svg";
 import type { StaticImageData } from "next/image";
 
 export type Floors = {
-  number: number;
+  level: number;
   label: string;
   image: StaticImageData;
 }[];
 
 export const floors: Floors = [
-  { number: 8, label: "８階", image: floor8Image },
-  { number: 7, label: "７階", image: floor7Image },
-  { number: 6, label: "６階", image: floor6Image },
-  { number: 5, label: "５階", image: floor5Image },
-  { number: 1, label: "１階", image: floor1Image },
+  { level: 8, label: "８階", image: floor8Image },
+  { level: 7, label: "７階", image: floor7Image },
+  { level: 6, label: "６階", image: floor6Image },
+  { level: 5, label: "５階", image: floor5Image },
+  { level: 1, label: "１階", image: floor1Image },
 ];
 
 export function FloorGuide() {
   const [selectedFloor, setSelectedFloor] = useState<number | null>(
-    floors.at(-1)?.number ?? null,
+    floors.at(-1)?.level ?? null,
   );
   return (
     <section className="flex flex-col items-center gap-16 pt-8 pb-16">
@@ -39,38 +39,40 @@ export function FloorGuide() {
         </p>
         <ul className="flex flex-col">
           {floors.map((floor) => (
-            <li
-              className="flex flex-row items-center gap-14"
-              key={floor.number}
-            >
+            <li className="flex flex-row items-center gap-14" key={floor.level}>
               <div className="space-x-4">
                 <span
                   className={cn(
                     "text-2xl",
-                    selectedFloor !== floor.number && "opacity-0",
+                    selectedFloor !== floor.level && "opacity-0",
                   )}
                 >
                   ▶︎
                 </span>
                 <label
-                  htmlFor={floor.number.toString()}
+                  htmlFor={floor.level.toString()}
                   className="cursor-pointer text-xl underline underline-offset-4"
                 >
                   <DotText>{floor.label}</DotText>
                 </label>
               </div>
               <button
-                id={floor.number.toString()}
-                aria-pressed={selectedFloor === floor.number}
+                id={floor.level.toString()}
+                aria-pressed={selectedFloor === floor.level}
                 className={cn(
                   "h-[2.8293rem] cursor-pointer transition-transform",
-                  selectedFloor === floor.number &&
+                  selectedFloor === floor.level &&
                     "-translate-x-5 translate-y-3",
                 )}
-                style={{ zIndex: floor.number }}
-                onClick={() => setSelectedFloor(floor.number)}
+                style={{ zIndex: floor.level }}
+                onClick={() => setSelectedFloor(floor.level)}
               >
-                <Image src={floor.image} alt={"フロア"} className="w-33.5" />
+                <Image
+                  src={floor.image}
+                  alt={"フロア"}
+                  className="w-33.5"
+                  draggable={false}
+                />
               </button>
             </li>
           ))}
