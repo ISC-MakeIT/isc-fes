6dae1f8cf49f101e0c2afa0ba98c97fb123393b8
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-11 16:27:36 +0900

    refactor: assetsを適切な場所に移動

--- frontend/src/app/(guest)/_ui/guest-footer.tsx
+++ frontend/src/app/(guest)/_ui/guest-footer.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import appLogo from "../_assets/app-logo.svg";
+import appLogo from "./assets/app-logo.svg";
 import { DotText } from "@/shared/ui/dot-text";
 import Link from "next/link";
 import { homeUrl, ordersUrl } from "@/shared/config";
