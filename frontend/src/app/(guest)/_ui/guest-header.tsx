e0fff60f7b3984cbf2e8adeb8c49eaea461e8454
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-11 16:54:41 +0900

    refactor: 可用性を下げないためにcookieの有無だけで判定

--- frontend/src/app/(guest)/_ui/guest-header.tsx
+++ frontend/src/app/(guest)/_ui/guest-header.tsx
@@ -3,11 +3,14 @@ import appLogo from "./assets/app-logo.svg";
 import homeLogo from "./assets/home-icon.svg";
 import { DotText } from "@/shared/ui/dot-text";
 import Link from "next/link";
-import { storeListUrl } from "@/shared/config";
-import { fetchCurrentAccount } from "@/entities/account";
+import { ACCOUNT_SESSION_COOKIE_NAME, storeListUrl } from "@/shared/config";
+import { cookies } from "next/headers";
 
 export async function GuestHeader() {
-  const account = await fetchCurrentAccount();
+  // 可用性やAPIコストを考えて、ここでは実APIを叩かずcookieの有無のみを見る
+  // セッションの検証はリダイレクト先で行う想定
+  const cookiesStore = await cookies();
+  const hasAccountSession = cookiesStore.has(ACCOUNT_SESSION_COOKIE_NAME);
   return (
     <header className="bg-primary text-background flex flex-row items-center justify-between px-6 py-4">
       <div className="flex flex-row gap-1">
@@ -19,9 +22,9 @@ export async function GuestHeader() {
         <DotText className="text-[1.375rem]">アプリ名</DotText>
       </div>
 
-      {account && (
+      {hasAccountSession && (
         <Link href={storeListUrl()} className="flex flex-row gap-2">
-          <Image src={homeLogo} alt="ストアのロゴ" />
+          <Image src={homeLogo} alt="" />
           <p className="text-sm">店舗へ</p>
         </Link>
       )}
