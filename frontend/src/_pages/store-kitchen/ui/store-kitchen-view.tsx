58d28a2c5d2c2db45197bd3a6307903d996a9f06
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-11 19:04:22 +0900

    feat: 空のページ用意

---
+++ frontend/src/_pages/store-kitchen/ui/store-kitchen-view.tsx
@@ -0,0 +1,3 @@
+export function StoreKitchenView() {
+  return <div></div>;
+}
---
+++ frontend/src/app/member/stores/[storeId]/kitchen/page.tsx
@@ -0,0 +1,5 @@
+import { StoreKitchenView } from "@/_pages/store-kitchen";
+
+export default function KitchenPage() {
+  return <StoreKitchenView />;
+}
