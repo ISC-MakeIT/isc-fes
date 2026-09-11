75886aeff2915853951d0555b8892e05b93c8d73
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-11 16:41:59 +0900

    feat: メンバーのページへ戻るボタンを追加

--- frontend/src/app/(guest)/_ui/guest-header.tsx
+++ frontend/src/app/(guest)/_ui/guest-header.tsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
 import appLogo from "./assets/app-logo.svg";
+import homeLogo from "./assets/home-icon.svg";
 import { DotText } from "@/shared/ui/dot-text";
+import Link from "next/link";
+import { storeListUrl } from "@/shared/config";
+import { fetchCurrentAccount } from "@/entities/account";
 
-export function GuestHeader() {
+export async function GuestHeader() {
+  const account = await fetchCurrentAccount();
   return (
-    <header className="bg-primary text-background grid grid-cols-[1fr_auto_1fr] items-center gap-1 px-8 py-4 md:flex md:flex-row">
-      <Image src={appLogo} alt={"アプリのロゴ"} className="justify-self-end" />
-      <DotText className="text-[1.375rem]">アプリ名</DotText>
-      <div aria-hidden="true" />
+    <header className="bg-primary text-background flex flex-row items-center justify-between px-6 py-4">
+      <div className="flex flex-row gap-1">
+        <Image
+          src={appLogo}
+          alt={"アプリのロゴ"}
+          className="justify-self-end"
+        />
+        <DotText className="text-[1.375rem]">アプリ名</DotText>
+      </div>
+
+      {account && (
+        <Link href={storeListUrl()} className="flex flex-row gap-2">
+          <Image src={homeLogo} alt="ストアのロゴ" />
+          <p className="text-sm">店舗へ</p>
+        </Link>
+      )}
     </header>
   );
 }
