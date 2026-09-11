0169cdc88cfdb3401a4a55ebb74c18f31d0a7c9f
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-11 18:48:36 +0900

    refactor: わかりやすくリネーム

--- frontend/src/shared/config/constants/urls.ts
+++ frontend/src/shared/config/constants/urls.ts
@@ -5,14 +5,17 @@ export const storeInvitationsUrl = (invitationId: string) =>
   `/invites/${invitationId}`;
 
 // 店舗ページ
-// TODO: 引数名のidをわかりやすい名前にする
 export const storeListUrl = () => "/member/stores";
 export const registerStoreUrl = () => "/member/stores/register";
-export const storeHomeUrl = (id: string) => `/member/stores/${id}`;
-export const storeKitchenUrl = (id: string) => `/member/stores/${id}/kitchen`;
-export const storePickupUrl = (id: string) => `/member/stores/${id}/pickup`;
-export const storeCallUrl = (id: string) => `/member/stores/${id}/call`;
-export const storeMenusUrl = (id: string) => `/member/stores/${id}/menus`;
+export const storeHomeUrl = (storeId: string) => `/member/stores/${storeId}`;
+export const storeKitchenUrl = (storeId: string) =>
+  `/member/stores/${storeId}/kitchen`;
+export const storePickupUrl = (storeId: string) =>
+  `/member/stores/${storeId}/pickup`;
+export const storeCallUrl = (storeId: string) =>
+  `/member/stores/${storeId}/call`;
+export const storeMenusUrl = (storeId: string) =>
+  `/member/stores/${storeId}/menus`;
 
 // 管理者ページ
 export const storeApplicationsUrl = () => "/admin/store-applications";
