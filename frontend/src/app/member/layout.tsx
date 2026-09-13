3b6a23e37d1872fb1fdea8d05692914142ddd968
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-13 12:09:16 +0900

    feat: layoutでは現在のURLを読む手段がないので、一旦ログイン後はstoreListに飛ばす

--- frontend/src/app/member/layout.tsx
+++ frontend/src/app/member/layout.tsx
@@ -1,6 +1,6 @@
 import { currentAccountQueryOptions } from "@/entities/account";
 import { createQueryClient } from "@/shared/api";
-import { loginUrl } from "@/shared/config";
+import { loginUrl, storeListUrl } from "@/shared/config";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { redirect } from "next/navigation";
 
@@ -9,8 +9,14 @@ import { redirect } from "next/navigation";
  */
 export default async function MemberLayout(props: LayoutProps<"/">) {
   const queryClient = createQueryClient();
-  const data = await queryClient.fetchQuery(currentAccountQueryOptions());
-  if (!data) redirect(loginUrl());
+  const account = await queryClient.fetchQuery(currentAccountQueryOptions());
+
+  if (!account) {
+    // TODO: 直前にいたページへ戻したい
+    //       layoutで今のURLを取得するスマートなやり方がなさそうなので、proxy.tsに移すかもしれない
+    const redirectTo = storeListUrl();
+    redirect(loginUrl(redirectTo));
+  }
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
