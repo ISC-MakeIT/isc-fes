cf4a33e845be9ccc16e910ec334f0773e8e07f53
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-23 20:39:22 +0900

    feat: カスタマイズがない時の表示

--- frontend/src/_pages/store-menus/ui/menu-form-fields.tsx
+++ frontend/src/_pages/store-menus/ui/menu-form-fields.tsx
@@ -153,6 +153,19 @@ export const MenuFormFields = withForm({
           children={(field) => (
             <Field>
               <FieldLabel className="text-xl">カスタマイズ</FieldLabel>
+              <p>
+                {toppings.length === 0 ? (
+                  <p>
+                    カスタマイズが登録されていません
+                    <br />
+                    「カスタマイズの追加」から登録してください。
+                  </p>
+                ) : (
+                  <p>
+                    このメニューに適用可能なカスタマイズを選択してください。
+                  </p>
+                )}
+              </p>
               <FieldContent>
                 <ToggleGroup
                   multiple
