ae6ea780e964fafc501c413595292bb4b785229b
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-24 10:51:21 +0900

    feat: もしアクセスされてもNotFoundにする

--- frontend/src/app/member/stores/[storeId]/menus/page.tsx
+++ frontend/src/app/member/stores/[storeId]/menus/page.tsx
@@ -1,8 +1,31 @@
 import { StoreMenusView } from "@/_pages/store-menus";
+import { currentAccountQueryOptions } from "@/entities/account";
+import {
+  storeMemberByAccountIdQueryOptions,
+  StoreMemberRole,
+} from "@/entities/store-member";
+import { createQueryClient } from "@/shared/api";
+import { loginUrl, storeListUrl } from "@/shared/config";
+import { notFound, redirect } from "next/navigation";
 
 export default async function StoreMenusPage({
   params,
 }: PageProps<"/member/stores/[storeId]/menus">) {
   const { storeId } = await params;
+
+  const queryClient = createQueryClient();
+  const account = await queryClient.fetchQuery(currentAccountQueryOptions());
+  if (!account) {
+    redirect(loginUrl(storeListUrl()));
+  }
+
+  const currentMember = await queryClient.fetchQuery(
+    storeMemberByAccountIdQueryOptions(storeId, account.id),
+  );
+
+  if (currentMember.role === StoreMemberRole.Staff) {
+    notFound();
+  }
+
   return <StoreMenusView storeId={storeId} />;
 }
