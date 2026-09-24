7ef4bf6da8066a51350e60c4f681bb90ccd14b4c
ryotaKFC <ryotamokatoka@gmail.com>
2026-09-24 11:42:45 +0900

    fix: スマホ対応

--- frontend/src/_pages/coming-soon/ui/coming-soon-view.tsx
+++ frontend/src/_pages/coming-soon/ui/coming-soon-view.tsx
@@ -1,8 +1,8 @@
 export function ComingSoonView() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-10">
-      <h1 className="text-3xl font-bold">このページは開発中です</h1>
-      <p>公開までしばらくお待ちください</p>
+      <h1 className="text-xl font-bold sm:text-3xl">このページは開発中です</h1>
+      <p className="text-sm sm:text-lg">公開までしばらくお待ちください</p>
     </div>
   );
 }
